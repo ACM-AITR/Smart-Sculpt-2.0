1d8ba7fb30e21a3c0d63df142b0ccf83d7c3975b
somtyagi20 <102314050+somtyagi20@users.noreply.github.com>
2024-04-01 01:07:27 +0530

    Update 05 IR_Sensor.ino

--- Day 1/05 IR Sensor.ino
+++ Day 1/05 IR Sensor.ino
@@ -1,1 +1,15 @@
-// Paste your code here
+int in1=2;
+int in2=3;
+void setup() {
+  pinMode(in1, INPUT);
+  pinMode(in2, OUTPUT);
+}
+
+void loop() {
+  int tsop = digitalRead(in1);
+  if(tsop == HIGH){
+    digitalWrite(in2,HIGH);
+  }else{
+    digitalWrite(in2,LOW);
+  }
+}
