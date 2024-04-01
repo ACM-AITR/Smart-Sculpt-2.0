76e0faa7334cb9d04f4ad899f448090779e3d19c
somtyagi20 <102314050+somtyagi20@users.noreply.github.com>
2024-04-01 08:45:18 +0530

    Update 01 blink.ino

--- Day 1/01 blink.ino
+++ Day 1/01 blink.ino
@@ -1,1 +1,11 @@
-// Paste your code here
+int in1=2;
+void setup() {
+  pinMode(in1, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(in1,HIGH);
+    delay(2000);
+    digitalWrite(in1,LOW);
+    delay(1000);
+}
