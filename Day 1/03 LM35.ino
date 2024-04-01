4dbee4b608a680e958539125dcd7916cdbc8c038
somtyagi20 <102314050+somtyagi20@users.noreply.github.com>
2024-04-01 09:07:05 +0530

    Update 03 LM35.ino

--- Day 1/03 LM35.ino
+++ Day 1/03 LM35.ino
@@ -1,1 +1,17 @@
-// Paste your code here
+int in1=34;
+int in2=26;
+void setup() {
+  Serial.begin(9600);
+  pinMode(in1, INPUT);
+  pinMode(in2,OUTPUT);
+}
+
+void loop() {
+int lm35 = analogRead(in1);
+Serial.print("LM35 : ");
+Serial.println(lm35);
+if(lm35 < 1500){
+  digitalWrite(in2,HIGH);
+}
+delay(1000);
+}
