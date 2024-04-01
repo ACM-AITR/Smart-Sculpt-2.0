290609cf35289d45bd1e8504f0b7b3de8ec07256
somtyagi20 <102314050+somtyagi20@users.noreply.github.com>
2024-04-01 09:00:02 +0530

    Update 02 LDR Sensor.ino

--- Day 1/02 LDR Sensor.ino
+++ Day 1/02 LDR Sensor.ino
@@ -1,1 +1,12 @@
-// Paste your code here
+int in1=34;
+void setup() {
+  Serial.begin(9600);
+  pinMode(in1, INPUT);
+}
+
+void loop() {
+int ldr = analogRead(in1);
+Serial.print("LDR : ");
+Serial.println(ldr);
+delay(1000);
+}
