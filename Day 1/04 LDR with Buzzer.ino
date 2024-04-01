0bce212f711dbad4df85ddbd71dfcc04fcdbe05f
somtyagi20 <102314050+somtyagi20@users.noreply.github.com>
2024-04-01 09:05:46 +0530

    Update 04 LDR with Buzzer.ino

--- Day 1/04 LDR with Buzzer.ino
+++ Day 1/04 LDR with Buzzer.ino
@@ -1,1 +1,17 @@
-// Paste your code here
+int in1=34;
+int in2=26;
+void setup() {
+  Serial.begin(9600);
+  pinMode(in1, INPUT);
+  pinMode(in2,OUTPUT);
+}
+
+void loop() {
+int ldr = analogRead(in1);
+Serial.print("LDR : ");
+Serial.println(ldr);
+if(ldr < 1500){
+  digitalWrite(in2,HIGH);
+}
+delay(1000);
+}// Paste your code here
